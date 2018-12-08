fb012fa80dae3819e4880dec4d444580c50c71b6
vivek rautela <vivekrautela000@gmail.com>
2018-12-08 13:11:42 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $gclient=new Google_Client();
 $gclient->setClientId('107598900486-hfp16n38eva2gbpfvhejd2ssmgstkukm.apps.googleusercontent.com');
 $gclient->setClientSecret('Fu3Cx4YfLsEbIQxQHEW_GHj2');
 $gclient->setApplicationName('google login');
-$gclient->setRedirectUri('http://localhost/fantasy_site/g-callback.php');
+$gclient->setRedirectUri('https://fantrick.herokuapp.com/g-callback.php');
 $gclient->addScope('https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email');
 
  ?>
