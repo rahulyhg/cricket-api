c8cfb7f4b3adca13c68613d69c5475b462ebbff5
vivek rautela <vivekrautela000@gmail.com>
2019-01-25 14:05:16 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 require_once('GoogleApi/vendor/autoload.php');
 $gclient=new Google_Client();
-$gclient->setClientId('107598900486-hfp16n38eva2gbpfvhejd2ssmgstkukm.apps.googleusercontent.com');
-$gclient->setClientSecret('Fu3Cx4YfLsEbIQxQHEW_GHj2');
+$gclient->setClientId('319379515687-abekk33h5nl6e2uob6mikua6qh08vfbp.apps.googleusercontent.com');
+$gclient->setClientSecret('QhT0cbjWMwubx3rlvewcuj69');
 $gclient->setApplicationName('google login');
-$gclient->setRedirectUri('https://github.com/vivekcoder123/cricket-api/g-callback.php');
+$gclient->setRedirectUri('https://fantrick.herokuapp.com/g-callback.php');
 $gclient->addScope('https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email');
 
  ?>
